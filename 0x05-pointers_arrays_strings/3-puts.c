12ba42eb52aabe6d4f63c67d7adef07e5e16df3e
davidmunene <davidmunene.dm@gmail.com>
2022-06-13 19:13:34 +0000

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - Print a string to stdout
+ * @str: address of string passed as argument
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
