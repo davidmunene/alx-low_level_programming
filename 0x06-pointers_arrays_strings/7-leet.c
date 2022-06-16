d6a5d4985a949871474c358f3d5cf5ad320094de
davidmunene <davidmunene.dm@gmail.com>
2022-06-16 20:01:45 +0000

    More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: string
+ *
+ * Return: s
+ */
+char *leet(char *str)
+{
+	int j, k;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (j = 0; *(str + j); j++)
+	{
+		for (k = 0; k <= 9; k++)
+		{
+			if (a[k] == *(str + j))
+				*(str + j) = b[k];
+		}
+	}
+	return (str);
+}
