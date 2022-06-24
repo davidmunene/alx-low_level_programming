e5401251da012b0e2a3cdcea1130ae2ecb264408
davidmunene <davidmunene.dm@gmail.com>
2022-06-24 20:53:58 +0000

    C - argc, argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point : Add Positive Numbers
+ * @argc: Number of Command Line Arguments
+ * @argv: Array that contains the program Command Line Arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int j;
+	int add = 0;
+
+	if (argc < 2)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+
+	for (j = 1; j < argc; j++)
+	{
+		if (!atoi(argv[j]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		add += atoi(argv[j]);
+	}
+
+	printf("%d\n", add);
+
+	return (0);
+}
