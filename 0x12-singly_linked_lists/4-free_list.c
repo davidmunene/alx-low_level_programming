92470dc560fc1082169d15ee791e0de667cf71e8
davidmunene <davidmunene.dm@gmail.com>
2022-07-14 19:13:24 +0000

    Singly linked lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: head of the linked list.
+ * Return: no return.
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		free(temp->str);
+		head = temp->next;
+		free(temp);
+	}
+}
